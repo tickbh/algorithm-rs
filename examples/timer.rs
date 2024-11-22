ef2821021184ad44d72bbdf40d01f1372b9935d2
tickbh <tickdream125@hotmail.com>
2024-11-22 15:59:12 +0800

    fix test

--- examples/timer.rs
+++ examples/timer.rs
@@ -19,15 +19,15 @@ fn main() {
     assert_eq!(timer.get_delay_id(), 1);
 
     let val = timer.update_deltatime(30).unwrap();
-    assert_eq!(val, vec![1, 30]);
+    assert_eq!(val.iter().map(|(_, v)| *v).collect::<Vec<usize>>(), vec![1, 30]);
 
     timer.add_timer(2);
 
     let val = timer.update_deltatime(119).unwrap();
-    assert_eq!(val, vec![2, 149]);
+    assert_eq!(val.iter().map(|(_, v)| *v).collect::<Vec<usize>>(), vec![2, 149]);
 
     let val = timer.update_deltatime(1).unwrap();
-    assert_eq!(val, vec![150]);
+    assert_eq!(val.iter().map(|(_, v)| *v).collect::<Vec<usize>>(), vec![150]);
     
     assert!(timer.is_empty());
 }
